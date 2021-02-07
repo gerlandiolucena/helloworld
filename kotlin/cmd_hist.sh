89d112601a90b1620ffd43a28f92c4ab66632c0e
Gerlandio Lucena <gerlandio.lucena@gmail.com>
2021-02-07 22:33:32 +0000

    Adicionando arquivo php

--- kotlin/cmd_hist.sh
+++ kotlin/cmd_hist.sh
@@ -1,16 +1,16 @@
-   43  git add .
-   44  git commit -m "Ajustando arquivo .cs"
-   45  git push
-   46  kotlin
-   47  pacman -Ss kotlin
-   48  sudo pacman -S kotlin
-   50  cd ..
-   52  mkdir kotlin
-   53  cd kotlin
-   54  touch hello_world.kt
-   55  curl -s https://get.sdkman.io | bash
-   56  source "/home/manjaro/.sdkman/bin/sdkman-init.sh"
-   57  sdk install kotlin
-   59  kotlinc hello_world.kt -include-runtime -d hello_world.jar
-   60  ls
    61  java -jar hello_world.jar
+   66  git status
+   68  git commit -m "Adicionando arquivo kotlin"
+   69  git push
+   70  pacman -Ss php
+   71  sudo pacman -S php
+   73  mkdir php 
+   74  cd php
+   75  touch hello_world.php
+   77  php hello_world.php
+   80  git status 
+   81  git add .
+   82  cd ..
+   83  cd helloworld
+   85  cd kotlin
+   86  ls
---
+++ php/hello_world.php
@@ -0,0 +1,7 @@
+<?php 
+
+function hello_world() {
+    echo "Hello world, for the language that helps to pay the bills.";
+}
+
+hello_world();
